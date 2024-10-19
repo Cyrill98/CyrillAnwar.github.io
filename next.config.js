b554fff944161d7244d03dca47d102032b4bc7dc
Cyrill <cyrill.norazman@maybank.com>
2024-10-19 14:15:40 +0800

    adjust config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const path = require('path')
  
 module.exports = {
-  output: "export",  // <=== enables static exports
-  reactStrictMode: true,
-
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },

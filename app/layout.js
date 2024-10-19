66f08070144d1fdb612dd5a5e26012898b3c2c90
Cyrill <cyrill.norazman@maybank.com>
2024-10-19 16:25:43 +0800

    update metadata tag

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Cyrill Anwar",
   description:
-    "This is the portfolio of Cyrill Anwar. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "Hi, My name is Cyrill 👋 I am a Software Engineer :) ",
 };
 
 export default function RootLayout({ children }) {

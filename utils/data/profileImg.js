183a70f5dc8a6f8b30bcdee7e309ca6422b44b17
Cyrill <cyrill.norazman@maybank.com>
2024-10-26 21:56:10 +0800

    carousel image

---
+++ utils/data/profileImg.js
@@ -0,0 +1,18 @@
+export const profileImg = [
+    {
+        id: 1,
+        img: '/profile.jpeg',
+    },
+    {
+        id: 2,
+        img: '/profile2.jpeg',
+    },
+    {
+        id: 3,
+        img: '/profile3.jpeg',
+    },
+    // {
+    //     id: 4,
+    //     img: '/profile4.jpeg',
+    // }
+]
